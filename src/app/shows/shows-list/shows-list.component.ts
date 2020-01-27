6e03395949c3e9794789ca2fb224d3060303c700
Natalia Gurgul <natalia.gurgul@wolterskluwer.com>
2020-01-27 09:55:36 +0100

    Added shows module with shows list component

---
+++ src/app/shows/shows-list/shows-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shows-list',
+  templateUrl: './shows-list.component.html',
+  styleUrls: ['./shows-list.component.less']
+})
+export class ShowsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+}
---
+++ src/app/shows/shows.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShowsListComponent } from './shows-list/shows-list.component';
+
+@NgModule({
+  declarations: [ShowsListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ShowsModule { }
