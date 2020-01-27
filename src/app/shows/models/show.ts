f84d065a629877f4ce6551c500b6332870aa4275
Natalia Gurgul <natalia.gurgul@wolterskluwer.com>
2020-01-27 12:22:08 +0100

    Added show interface

---
+++ src/app/shows/models/show.ts
@@ -0,0 +1,48 @@
+export interface Show {
+  id: number;
+  url: string;
+  name: string;
+  type: string;
+  language: string;
+  genres: Array<string>;
+  status: string;
+  runtime: number;
+  premiered: string;
+  officialSite: string;
+  schedule: {
+    time: string;
+    days: Array<string>;
+  };
+  rating: {
+    average: number;
+  };
+  weight: number;
+  network: {
+    id: number;
+    name: string;
+    country: {
+      name: string;
+      code: string;
+      timezone: string;
+    }
+  };
+  webChannel: null;
+  externals: {
+    tvrage: number;
+    thetvdb: number;
+    imdb: string;
+  };
+  image: {
+    medium: string;
+  };
+  summary: string;
+  updated: number;
+  _links: {
+    self: {
+      href: string;
+    },
+    previousepisode: {
+      href: string;
+    }
+  };
+}
